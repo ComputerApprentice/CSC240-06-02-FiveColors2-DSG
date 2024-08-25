37e6548745d27bbcf90439eba3cdbe6bf9959358
Dixon Geiser <DGEISE2490@my.sullivan.edu>
2024-08-25 00:14:26 -0400

    Added the reset button and the code to make it reset the form (no buttons selected and the background reverted back to its original form).

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace CSC240_06_02_FiveColors2_DSG
 {
@@ -19,27 +20,59 @@ namespace CSC240_06_02_FiveColors2_DSG
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.Color.White;
+            BackColor = System.Drawing.Color.White;
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.Color.Black;
+            BackColor = System.Drawing.Color.Black;
         }
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.Color.Red;
+            BackColor = System.Drawing.Color.Red;
         }
 
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.Color.Green;
+            BackColor = System.Drawing.Color.Green;
         }
 
         private void radioButton5_CheckedChanged(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.Color.Blue;
+            BackColor = System.Drawing.Color.Blue;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            RecursiveClearTextBoxes(this.Controls);
+            this.Controls.Clear();
+            this.InitializeComponent();
+            BackColor = DefaultBackColor;
+        }
+
+        private void RecursiveClearTextBoxes(Control.ControlCollection cc)
+        {
+            if (radWhite.Checked)
+            {
+                radWhite.Checked = false;
+            }
+            if (radBlack.Checked)
+            {
+                radBlack.Checked = false;
+            }
+            if (radRed.Checked)
+            {
+                radRed.Checked = false;
+            }
+            if (radGreen.Checked)
+            {
+                radGreen.Checked = false;
+            }
+            if (radBlue.Checked)
+            {
+                radBlue.Checked = false;
+            }
         }
     }
 }
